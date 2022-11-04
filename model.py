175b61fa4f65edf2b2a076b7afb2703869e145f8
gauravsett <gsett3@gatech.edu>
2022-11-04 10:50:41 -0400

    Update model wrappers

--- model.py
+++ model.py
@@ -1,36 +1,53 @@
-from secrets import OPENAI_API_KEY
+import json
 import openai
 from openai import Completion
+from transformers import pipeline, set_seed
 
 
 class Model():
     
     def __init__(self) -> None:
-        pass
+        raise NotImplementedError
     
     def generate(self, input: str) -> str:
-        pass
+        raise NotImplementedError
     
-    def classify(self, input: str, classes: list = [0, 1]) -> int:
-        pass
+    def classify(self, input: str) -> int:
+        raise NotImplementedError
 
 
-class GPT3(Model):
+class OpenAIModel(Model):
     
     def __init__(self, version: str) -> None:
-        openai.api_key = OPENAI_API_KEY
+        with open("secrets.json", "r") as f:
+            openai.api_key = json.load(f)["OPENAI_API_KEY"]
         self.version = version
     
     def generate(self, input: str) -> str:
         response = Completion.create(
-            model=self.version, prompt=input, temperature=0, max_tokens=256
+            model=self.version, prompt=input, temperature=0, max_tokens=128
         )
-        return response.choices[0].text
+        return response.choices[0].text.strip()
     
-    def classify(self, input: str, classes: list = [0, 1]) -> int:
+    def classify(self, input: str) -> int:
         response = Completion.create(
-            model=self.version, prompt=input, temperature=0, max_tokens=1
+            model=self.version, prompt=input, temperature=0, max_tokens=1, logprobs=1
         )
-        code = {"yes": 1, "no": 0}
-        return code[response.choices[0].text.lower()]
-   
+        return response.choices[0].text.strip()
+
+
+class HuggingFaceModel(Model):
+    
+    def __init__(self, version: str) -> None:
+        self.version = version
+        self.pipeline = pipeline("text-generation", model=self.version)
+        set_seed(7)
+    
+    def generate(self, input: str) -> str:
+        response = self.pipeline(input, max_new_tokens=128, do_sample=False)
+        return response[0]["generated_text"][len(input):].strip()
+    
+    def classify(self, input: str) -> int:
+        response = self.pipeline(input, max_new_tokens=1, do_sample=False)
+        return response[0]["generated_text"][len(input):].strip()
+    
