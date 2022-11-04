2707a1f9cf3b22412ad469ce5ff1d86d4851ae3b
gauravsett <gsett3@gatech.edu>
2022-11-04 14:41:46 -0400

    Create torch data class

--- data.py
+++ data.py
@@ -1,30 +1,37 @@
 import os
+from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 
-
-class Data():
-    
-    def __init__(self) -> None:
-        pass
-        
-    def load_data(self) -> pd.DataFrame:
-        pass
-
-
-class CommonsenseData(Data):
+class CommonsenseDataset(Dataset):
     
     def __init__(self) -> None:
         self.path = "data/commonsense/"
-        self.data = self.load_data()
+        self.data = self._load_data()
+        
+    def __len__(self) -> int:
+        return len(self.data)
     
-    def load_data(self) -> pd.DataFrame:
+    def __getitem__(self, index: int) -> dict:
+        return {
+            "input": self.data.iloc[index]["input"],
+            "label": self.data.iloc[index]["label"],
+        }
+    
+    def _load_data(self) -> pd.DataFrame:
         data = pd.DataFrame(
-            columns=["label", "input", "is_short", "edited", "subset"]
+            columns=["label", "input", "is_short", "edited"]
         )
         for file in os.listdir(self.path):
             if "ambig" in file:
                 continue
             subset = pd.read_csv(self.path + file)
-            subset["subset"] = file.split("_", maxsplit=1)[1][:-4]
             data = pd.concat([data, subset]).reset_index(drop=True)
+        data = data.loc[data["is_short"] == True][["label", "input"]]
         return data
+
+
+commonsense_dataloader = DataLoader(
+    CommonsenseDataset(),
+    batch_size=1,
+    shuffle=False,
+)
--- model.py
+++ model.py
@@ -31,23 +31,46 @@ class OpenAIModel(Model):
     
     def classify(self, input: str) -> int:
         response = Completion.create(
-            model=self.version, prompt=input, temperature=0, max_tokens=1, logprobs=1
+            model=self.version, 
+            prompt=input, 
+            temperature=0, 
+            max_tokens=1, 
+            logprobs=1
         )
         return response.choices[0].text.strip()
 
 
 class HuggingFaceModel(Model):
     
-    def __init__(self, version: str) -> None:
+    def __init__(
+        self, 
+        version: str, 
+        device: str="cpu", 
+        batch_size: int=1
+    ) -> None:
         self.version = version
-        self.pipeline = pipeline("text-generation", model=self.version)
-        set_seed(7)
+        self.pipeline = pipeline(
+            "text-generation", 
+            model=self.version, 
+            device=device, 
+            batch_size=batch_size,
+        )
+        set_seed(0)
     
-    def generate(self, input: str) -> str:
-        response = self.pipeline(input, max_new_tokens=128, do_sample=False)
-        return response[0]["generated_text"][len(input):].strip()
+    def generate(self, input: list) -> str:
+        response = self.pipeline(
+            input, 
+            max_new_tokens=128, 
+            return_full_text=False,
+            do_sample=False,
+        )
+        return [r["generated_text"].strip() for r in response]
     
-    def classify(self, input: str) -> int:
-        response = self.pipeline(input, max_new_tokens=1, do_sample=False)
-        return response[0]["generated_text"][len(input):].strip()
-    
+    def classify(self, input) -> int:
+        response = self.pipeline(
+            input, 
+            max_new_tokens=1, 
+            return_full_text=False,
+            do_sample=False,
+        )
+        return [r["generated_text"].strip() for r in response]
